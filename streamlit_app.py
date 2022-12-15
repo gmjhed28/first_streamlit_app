0b6470d7fb23e99a8efdb7384594a42ff12d3740
gmjhed28 <knightfury28@gmail.com>
2022-12-15 18:32:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
 
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
@@ -33,8 +37,7 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
   else:
-      fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(fruityvice_normalized)
       
 except URLError as e:
